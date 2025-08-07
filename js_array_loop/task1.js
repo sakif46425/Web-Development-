4ba8c61126f2c7d3bfb3a0262e007a791862fa51
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-07 16:11:14 +0600

    Add the js_array_loop folder and aslo add task1.js file

---
+++ js_array_loop/task1.js
@@ -0,0 +1,12 @@
+const color = ['red', 'blue', 'green', 'yellow', 'organge'];
+//const reversedcolors = [];
+
+// for (let i = color.length - 1; i >= 0; i--)
+// {
+//     reversedcolors.push(color[i]);
+// }
+
+// console.log(reversedcolors);
+
+const reversedColors = color.reverse();
+console.log(reversedColors);
