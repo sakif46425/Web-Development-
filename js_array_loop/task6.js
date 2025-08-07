e7fbccfd62e49480f5228bae2e373c170fe5ce86
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-07 17:04:42 +0600

    Add the task6.js file

---
+++ js_array_loop/task6.js
@@ -0,0 +1,8 @@
+const students = [
+    {name: 'sakif', marks: 85},
+    {name: 'Tousif', marks: 90}
+];
+
+for (const student of students) {
+    console.log("Name: ", student.name, "| Marks: ", student.marks);
+}
