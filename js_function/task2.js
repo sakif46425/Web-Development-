44fd4d84d82a34bfe2abb6adad41e9a20429dcc1
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-08 11:39:05 +0600

    Add the task2.js file

---
+++ js_function/task2.js
@@ -0,0 +1,16 @@
+function number (num)
+{
+    if (num % 2 === 0)
+    {
+        //console.log("Even number : ",num);
+        return num / 2;
+    }
+    else
+    {
+        //console.log("Odd number : ",num);
+        return num * 2;
+    }
+}
+
+console.log(number(20));
+console.log(number(15));
