39d574d256daf3c19d78c067bb097fb06e99a467
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-10 16:47:07 +0600

    Upload the js-problems-part1 folder and aslo upload the task1.js file

---
+++ js-problems-part1/task1.js
@@ -0,0 +1,11 @@
+function celsisusToFahrenheit(celsius)
+
+
+{
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+
+console.log(celsisusToFahrenheit(0));
+console.log(celsisusToFahrenheit(25));
+console.log(celsisusToFahrenheit(100));
