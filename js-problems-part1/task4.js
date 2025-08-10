48302d5f893344923b2b0fffb0b4db0fc38bf239
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-10 17:21:16 +0600

    Add the task4.js file

---
+++ js-problems-part1/task4.js
@@ -0,0 +1,18 @@
+function findlongestword(str)
+{
+    let words = str.split(" ");
+    let longestWord = " ";
+
+    for (let word of words)
+    {
+        if (word.length > longestWord.length)
+        {
+            longestWord = word;
+        }
+    }
+
+    return longestWord;
+}
+
+console.log(findlongestword("The quick brown fox jumps over the lazy dog"));
+console.log(findlongestword("JavaScript is a versatile programming language"));
